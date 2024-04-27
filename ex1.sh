56e77e80c1a15f31581fbca61cb798db07196a94
juancolchete <juanudkp@gmail.com>
2024-04-27 15:00:14 -0300

    feat: ex1.sh

---
+++ ex1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo -n "Qual e o seu nome: "
+read nome
+echo -n "Qual e a sua idade: "
+read idade
+echo -n "Qual e o seu cpf: "
+read cpf
+echo -n "Qual e o seu endereco: "
+read endereco
+
+echo "################"
+echo "nome: $nome"
+echo "idade: $idade"
+echo "cpf: $cpf"
+echo "endereco: $endereco"
+echo "################"
+
