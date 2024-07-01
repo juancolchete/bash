f568aa6abb0ac455a3fb7400cbd114c37ec3fbf0
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2024-07-01 19:57:11 -0300

    chore!: btrfs after gen jail

---
+++ archLinuxJail.sh
@@ -0,0 +1,10 @@
+su root
+pacman -S arch-chroot
+mkdir /mnt/neovimLab/
+mount -o subvol=@ /dev/sda2 /mnt/neovimLab
+mount -o subvol=@home /dev/sda2 /mnt/neovimLab/home
+mount -o subvol=@pkg /dev/sda2 /mnt/neovimLab/var/cache/pacman/pkg
+mount -o subvol=@log /dev/sda2 /mnt/neovimLab/var/log
+mount -o subvol=@.snapshots /dev/sda2 /mnt/neovimLab/.snapshots
+mount /dev/sda2 /mnt/neovimLab/boot
+arch-chroot /mnt/neovimLab
