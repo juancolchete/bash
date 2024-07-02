bd19fb764477ed9a951ca1273a32628aaa48b6d2
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2024-07-02 10:55:23 -0300

    chore: mount a truly development environment

--- archLinuxJail.sh
+++ archLinuxJail.sh
@@ -1,11 +1,6 @@
 su root
 pacman -S arch-install-scripts
-mkdir /mnt/neovimLab/
-cd /mnt/neovimLab/
-mount -o subvol=@ /dev/sda2 /mnt/neovimLab
-mount -o subvol=@home /dev/sda2 /mnt/neovimLab/home
-mount -o subvol=@pkg /dev/sda2 /mnt/neovimLab/var/cache/pacman/pkg
-mount -o subvol=@log /dev/sda2 /mnt/neovimLab/var/log
-mount -o subvol=@.snapshots /dev/sda2 /mnt/neovimLab/.snapshots
-mount /dev/sda2 /mnt/neovimLab/boot
-arch-chroot /mnt/neovimLab
+mkdir /neovimLab/
+pacstrap -K /neovimLab base base-devel
+mount --bind /neovimLab /neovimLab
+arch-chroot /neovimLab
