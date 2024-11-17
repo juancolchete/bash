b0d0cd8fc3da5e0cf51d9157b644c98375443560
juancolchete <juanudkp@gmail.com>
2024-11-17 10:40:20 -0300

    ci: jq sample

--- jq.sh
+++ jq.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 userCount=`cat sample.json | jq -r '.users | length'`
 echo userCount: $userCount
+userFriendsIds=`jq '.users[] | select(.name == "Jane")' sample.json | jq -r '.friends'`
+userFriends=`jq '.users[] | select(.name == "Jane")' sample.json | jq -r '.friends'`
+echo userFriends: $userFriends
+
