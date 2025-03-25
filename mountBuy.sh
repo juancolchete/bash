1ce48accae25cd6387064046bbc23845bc897814
juancolchete <juanudkp@gmail.com>
2025-03-25 01:57:15 -0300

    fix mount buy

--- mountBuy.sh
+++ mountBuy.sh
@@ -1,4 +1,6 @@
 source .env
 BUY_YEAR_RAW=$(curl -s -L $MB_DATA_API | jq -r .price)
 BUY_YEAR=$( printf "%.0f" $BUY_YEAR_RAW )
-echo "$((BUY_YEAR / YEAR_TO_DAY))"
+DAYS_PASSED=$((10#$(date +%j)))
+EXPECT_BUY=$((BUY_YEAR / YEAR_TO_DAY))
+echo "$((DAYS_PASSED*EXPECT_BUY))"
